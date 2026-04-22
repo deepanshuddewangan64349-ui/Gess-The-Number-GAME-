80db7dc9d2bef639c027e5f90803692584ef771f
deepanshuddewangan64349-ui <deepanshuddewangan64349@gmail.com>
2026-04-22 16:46:56 +0530

    set timeout

--- script.js
+++ script.js
@@ -7,26 +7,22 @@ let submitBtn = document.getElementById("submit");
 let resetBtn = document.getElementById("reset");
 let msg = document.getElementById("msg");
 
-submitBtn.addEventListener("click",() => {
-    msg.innerText = "";
-    setTimeout(()=>{
-        guessNum = inputVal.value;
-  if (guessNum > gameNum) {
-    msg.innerText = "you giving number is big. please guess again";
-  } else if (guessNum < gameNum) {
-    msg.innerText = "you giving number is small. please guess again";
-  } else {
-    msg.innerText = "congratulations, you guess the right number";
-  }
-    },100);
-  
+submitBtn.addEventListener("click", () => {
+  msg.innerText = "";
+  setTimeout(() => {
+    guessNum = inputVal.value;
+    if (guessNum > gameNum) {
+      msg.innerText = "you giving number is big. please guess again";
+    } else if (guessNum < gameNum) {
+      msg.innerText = "you giving number is small. please guess again";
+    } else {
+      msg.innerText = "congratulations, you guess the right number";
+    }
+  }, 100);
 });
 
-// function checkNumber() {
-  
-// }
-resetBtn.addEventListener("click",()=>{
-    msg.innerText = "";
-    inputVal.value = "0";
-    gameNum = Math.floor(Math.random() * 100) + 1;
+resetBtn.addEventListener("click", () => {
+  msg.innerText = "";
+  inputVal.value = "0";
+  gameNum = Math.floor(Math.random() * 100) + 1;
 });
